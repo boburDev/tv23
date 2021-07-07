8136c69f56b9eb2494953ba12f053530d23e5d77
boburDev <negmatov1907@gmail.com>
2021-07-07 19:43:34 +0500

    .

--- src/pages/categoryMovie/categoryMovie.jsx
+++ src/pages/categoryMovie/categoryMovie.jsx
@@ -24,7 +24,7 @@ export default function CategoryMovie() {
 
 			const movies = await axios.get(api + '/movie-category', {
 				params: {
-					categoryId: val
+					categoryName: val
 				}
 			})
 			setMovies(movies.data.data)
