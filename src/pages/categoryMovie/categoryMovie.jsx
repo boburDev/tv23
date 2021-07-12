26d241588343d5130b8695309f0664693223de4f
Murodjon Azamov <programmerislamic@gmail.com>
2021-07-12 19:08:22 +0500

    add some changes

--- src/pages/categoryMovie/categoryMovie.jsx
+++ src/pages/categoryMovie/categoryMovie.jsx
@@ -1,80 +1,74 @@
-import Footer from "../../component/footer/footer"
-import Ads from "../../component/ads/ads"
-import Navbar from "../../component/navbar/navbar"
-import TrillerCarousel from '../../component/triller/trillerCarousel/triller'
-import MovieCategory from '../../component/categories/categories'
-import { useEffect, useState } from "react"
-import axios from "axios"
-import { useApi } from "../../context/api"
-import { useParams } from "react-router-dom"
-
+import Footer from "../../component/footer/footer";
+import Ads from "../../component/ads/ads";
+import Navbar from "../../component/navbar/navbar";
+import TrillerCarousel from "../../component/triller/trillerCarousel/triller";
+import MovieCategory from "../../component/categories/categories";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useApi } from "../../context/api";
+import { useParams } from "react-router-dom";
 
 export default function CategoryMovie() {
-    const [api] = useApi()
-	const category = useParams()
-	const [movies, setMovies] = useState({})	
-	const [categories, setCategories] = useState()	
-	const [recommendedTriller, setRecommendedTriller] = useState([])
-	const [loading, setLoading] = useState(false)
+  const [api] = useApi();
+  const category = useParams();
+  const [movies, setMovies] = useState({});
+  const [categories, setCategories] = useState();
+  const [recommendedTriller, setRecommendedTriller] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-	async function getMovies (api, value){
-		try {
-			setLoading(true)
-			const val = value.category.toLowerCase().charAt(0).toUpperCase() + value.category.toLowerCase().slice(1)
+  async function getMovies(api, value) {
+    try {
+      setLoading(true);
+      const val =
+        value.category.toLowerCase().charAt(0).toUpperCase() +
+        value.category.toLowerCase().slice(1);
 
-			const movies = await axios.get(api + '/movie-category', {
-				params: {
-					categoryName: val
-				}
-			})
-			setMovies(movies.data.data)
-			setLoading(false)
-		} catch (error) {
-		}
-	}
+      const movies = await axios.get(api + "/movie-category", {
+        params: {
+          categoryName: val,
+        },
+      });
+      setMovies(movies.data.data);
+      setLoading(false);
+    } catch (error) {}
+  }
 
-	async function getCategories(api) {
-		try {
-			setLoading(true)
-			const categories = await axios.get(api + '/categories')
-			const data = categories.data.data
-			setCategories(data)
-			setLoading(false)
-		} catch (error) {
-			
-		}
-	}
+  async function getCategories(api) {
+    try {
+      setLoading(true);
+      const categories = await axios.get(api + "/categories");
+      const data = categories.data.data;
+      setCategories(data);
+      setLoading(false);
+    } catch (error) {}
+  }
 
-	async function recommendedTrillers(api) {
-		try {
-			setLoading(true)
-			const trillers = await axios.get(api + '/recommended-t')
-			setRecommendedTriller(trillers.data.data)
-			setLoading(false)
-		} catch (error) {
-			
-		}
-	}
-	useEffect(()=>{
-		getMovies(api, category)
-		recommendedTrillers(api)
-		getCategories(api)
-	},[api, category])
-	
+  async function recommendedTrillers(api) {
+    try {
+      setLoading(true);
+      const trillers = await axios.get(api + "/recommended-t");
+      setRecommendedTriller(trillers.data.data);
+      setLoading(false);
+    } catch (error) {}
+  }
+  useEffect(() => {
+    getMovies(api, category);
+    recommendedTrillers(api);
+    getCategories(api);
+  }, [api, category]);
 
-    return (
-        <>
-        <Navbar />
-        <TrillerCarousel
-		api={api}
-		movies={recommendedTriller} />
-		<MovieCategory
-		data={movies}
-		categories={categories}
-		loading={loading}
-		what="signle-movie-category" />
-		<Ads />
-		<Footer />
-        </>
-    )
+  return (
+    <>
+      <Navbar />
+      <TrillerCarousel api={api} movies={recommendedTriller} />
+      <MovieCategory
+        data={movies}
+        categories={categories}
+        loading={loading}
+        what="signle-movie-category"
+      />
+      <Ads />
+      <Footer />
+    </>
+  );
 }
