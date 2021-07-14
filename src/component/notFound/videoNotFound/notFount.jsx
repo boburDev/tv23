735d85b7f3d70c8ed25ec024fc12e20d2e413ebf
Murodjon Azamov <programmerislamic@gmail.com>
2021-07-14 23:04:20 +0500

    fix title box font size

--- src/component/notFound/videoNotFound/notFount.jsx
+++ src/component/notFound/videoNotFound/notFount.jsx
@@ -1,12 +1,14 @@
-import { useTheme } from '../../../context/theme'
-import st from './notFound.module.css'
+import { useTheme } from "../../../context/theme";
+import st from "./notFound.module.css";
 
 export default function NoFoundVideos() {
-    const [dark] = useTheme()
+  const [dark] = useTheme();
 
-    return (
-        <div className={st.container}>
-            <div className={st.text} style={{color:dark? ' ': 'black'}}>NO Found Videos</div>
-        </div>
-    )
+  return (
+    <div className={st.container}>
+      <div className={st.text} style={{ color: dark ? " " : "black" }}>
+        NO Found Videos
+      </div>
+    </div>
+  );
 }
