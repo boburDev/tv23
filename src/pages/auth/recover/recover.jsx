c8ee5b02fd31b86316a0a4a617ae829c330b9fe9
boburDev <negmatov1907@gmail.com>
2021-07-19 18:34:56 +0500

    Update recover.jsx

--- src/pages/auth/recover/recover.jsx
+++ src/pages/auth/recover/recover.jsx
@@ -2,13 +2,16 @@ import st from '../auth.module.css'
 import Navbar from "../../../component/navbar/navbar"
 import MaskLogin from '../../../assets/image/MaskLogin.png'
 import Recover from '../../../component/auth/recover/recover'
+import VerifyPhone from '../../../component/auth/verifyphone/verifyphone'
 import { useAuth } from '../../../context/user'
 import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { useLogin } from '../../../context/login'
 
 export default function Recovery () {
     const [auth] = useAuth()
     const language = useParams()
+    const [login] = useLogin()
 
     useEffect(()=>{
         if (typeof auth === 'object' && auth) {
@@ -22,7 +25,10 @@ export default function Recovery () {
             <Navbar path="auth" />
             <div className={st.area}>
                <div className={st.fields}>
-                    <Recover />
+                    
+                    {
+                    (login && login.recovery === 'rocover') ? <VerifyPhone /> :  <Recover /> 
+                }
                </div>
                <div className={st.mask}> 
                   <img src={MaskLogin} alt=""/>
